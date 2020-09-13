da729c77783ec094a1c3a4e08b506ccd1ada7371
Trevor <trz24@nau.edu>
2020-09-12 20:07:53 -0700

    stuff

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-const doorImage1 = document.getElementById('door1');
-const botDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/robot.svg";
-doorImage1.onclick = () => {
+let doorImage1 = document.getElementById('door1');
+let botDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/robot.svg";
+
+door1.onclick = () => {
   doorImage1.src = botDoorPath;
 }
