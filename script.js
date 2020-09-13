c9c38f663a9357614353397826be8601d7593f0d
Trevor <trz24@nau.edu>
2020-09-13 00:06:59 -0700

    Waypoint #1

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 let doorImage1 = document.getElementById('door1');
+let doorImage2 = document.getElementById('door2');
+let doorImage3 = document.getElementById('door3');
 const botDoorPath = 'https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/robot.svg';
+const beachDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/beach.svg";
+const spaceDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/space.svg";
 doorImage1.onclick = () => {
   door1.src= botDoorPath;
+}
+doorImage2.onclick = () => {
+  door2.src = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/beach.svg"
+}
+doorImage3.onclick = () => {
+  door3.src = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/space.svg"
 }
